angular-gantt/angular-gantt

commit d83f0e3dafd92b33dff5df9915d1967cfd5b7536

Better head-show-* options. fix #142

assets/angular-gantt.js
@@ -1192,7 +1192,7 @@ gantt.directive('gantt', ['Gantt', 'dateFunctions', 'mouseOffset', 'debounce', '
                     throw "Unsupported view scale: " + $scope.viewScale;
             }
 
-            self.headerGenerator = new HeaderGenerator.instance($scope.viewScale);
+            self.headerGenerator = new HeaderGenerator.instance($scope);
         };
         self.buildGenerators();
 
@@ -1767,35 +1767,21 @@ gantt.directive('gantt', ['Gantt', 'dateFunctions', 'mouseOffset', 'debounce', '
     };
 
     return {
-        instance: function(viewScale) {
+        instance: function($scope) {
             this.generate = function(columns) {
                 var headers = {};
-
-                switch(viewScale) {
-                    case 'hour':
-                        headers.hour = generateHourHeader(columns);
-                        headers.day = generateDayHeader(columns);
-
-                        break;
-                    case 'day':
-                        headers.day = generateDayHeader(columns);
-                        headers.week = generateWeekHeader(columns);
-                        headers.month = generateMonthHeader(columns);
-
-                        break;
-                    case 'week':
-                        headers.week = generateWeekHeader(columns);
-                        headers.month = generateMonthHeader(columns);
-
-                        break;
-                    case 'month':
-                        headers.month = generateMonthHeader(columns);
-
-                        break;
-                    default:
-                        throw "Unsupported view scale: " + viewScale;
+                if ($scope.headerShowHour && ['hour'].indexOf($scope.viewScale) > -1) {
+                    headers.hour = generateHourHeader(columns);
                 }
-
+                if ($scope.headerShowDay && ['hour', 'day'].indexOf($scope.viewScale) > -1) {
+                    headers.day = generateDayHeader(columns);
+                }
+                if ($scope.headerShowWeek && ['week'].indexOf($scope.viewScale) > -1) {
+                    headers.week = generateWeekHeader(columns);
+                }
+                if ($scope.headerShowMonth && ['week', 'month'].indexOf($scope.viewScale) > -1) {
+                    headers.month = generateMonthHeader(columns);
+                }
                 return headers;
             };
         }

src/logic/headerGenerator.factory.js
@@ -69,35 +69,21 @@ gantt.factory('HeaderGenerator', [ 'Column', 'dateFunctions', function (Column, 
     };
 
     return {
-        instance: function(viewScale) {
+        instance: function($scope) {
             this.generate = function(columns) {
                 var headers = {};
-
-                switch(viewScale) {
-                    case 'hour':
-                        headers.hour = generateHourHeader(columns);
-                        headers.day = generateDayHeader(columns);
-
-                        break;
-                    case 'day':
-                        headers.day = generateDayHeader(columns);
-                        headers.week = generateWeekHeader(columns);
-                        headers.month = generateMonthHeader(columns);
-
-                        break;
-                    case 'week':
-                        headers.week = generateWeekHeader(columns);
-                        headers.month = generateMonthHeader(columns);
-
-                        break;
-                    case 'month':
-                        headers.month = generateMonthHeader(columns);
-
-                        break;
-                    default:
-                        throw "Unsupported view scale: " + viewScale;
+                if ($scope.headerShowHour && ['hour'].indexOf($scope.viewScale) > -1) {
+                    headers.hour = generateHourHeader(columns);
                 }
-
+                if ($scope.headerShowDay && ['hour', 'day'].indexOf($scope.viewScale) > -1) {
+                    headers.day = generateDayHeader(columns);
+                }
+                if ($scope.headerShowWeek && ['week'].indexOf($scope.viewScale) > -1) {
+                    headers.week = generateWeekHeader(columns);
+                }
+                if ($scope.headerShowMonth && ['week', 'month'].indexOf($scope.viewScale) > -1) {
+                    headers.month = generateMonthHeader(columns);
+                }
                 return headers;
             };
         }

src/logic/gantt.factory.js
@@ -51,7 +51,7 @@ gantt.factory('Gantt', ['$filter', 'Row', 'Timespan', 'ColumnGenerator', 'Header
                     throw "Unsupported view scale: " + $scope.viewScale;
             }
 
-            self.headerGenerator = new HeaderGenerator.instance($scope.viewScale);
+            self.headerGenerator = new HeaderGenerator.instance($scope);
         };
         self.buildGenerators();
 

template/gantt.tmpl.html
@@ -3,7 +3,7 @@
          ng-style="(labelsWidth > 0 && {'width': labelsWidth+'px'} || {})"
          gantt-labels-resize="allowLabelsResizing" gantt-labels-resize-width="labelsWidth" gantt-labels-resize-min-width="50" on-labels-resized="raiseLabelsResized(width)" >
         <div class="gantt-labels-head"
-             ng-show="gantt.columns.length > 0">
+             ng-show="gantt.columns.length > 0 && gantt.getActiveHeadersCount() > 0">
             <div class="gantt-labels-head-row"
                  ng-click="raiseLabelHeaderClickedEvent($event)"
                  ng-dblclick="raiseLabelHeaderDblClickedEvent($event)"
@@ -32,12 +32,12 @@
         </div>
     </div>
     <div class="gantt-head"
-         ng-show="gantt.columns.length > 0">
+         ng-show="gantt.columns.length > 0 && gantt.getActiveHeadersCount() > 0">
         <div gantt-horizontal-scroll-receiver
              ng-style="{'position': 'relative', 'width': gantt.width+'em'}">
             <div class="gantt-head-row"
                  ng-class="(gantt.headers.month !== undefined && 'gantt-head-row-bottom' || '')"
-                 ng-if="headerShowMonth && gantt.headers.month !== undefined">
+                 ng-if="gantt.headers.month !== undefined">
                 <span ng-style="{'width': c.width+'em', 'left': c.left+'em'}"
                       ng-repeat="c in gantt.headers.month | ganttColumnLimit:scroll_start:scroll_width track by $index"
                       ng-click="raiseColumnDateClickedEvent($event, c)"
@@ -46,7 +46,7 @@
                     {{ headerFormatMonth && (c.date | ganttDate:headerFormatMonth)  || (c.date | ganttDate:'MMMM yyyy') }}
                 </span>
             </div>
-            <div class="gantt-head-row" ng-if="headerShowWeek && gantt.headers.week !== undefined">
+            <div class="gantt-head-row" ng-if="gantt.headers.week !== undefined">
                 <span ng-style="{'width': c.width+'em', 'left': c.left+'em'}"
                       ng-repeat="c in gantt.headers.week | ganttColumnLimit:scroll_start:scroll_width track by $index"
                       ng-click="raiseColumnDateClickedEvent($event, c)"
@@ -55,7 +55,7 @@
                     {{headerFormatWeek && (headerFormatWeek | sprintf:c.week) || c.week }}
                 </span>
             </div>
-            <div class="gantt-head-row" ng-if="headerShowDay && gantt.headers.day !== undefined">
+            <div class="gantt-head-row" ng-if="gantt.headers.day !== undefined">
                 <span ng-style="{'width': c.width+'em', 'left': c.left+'em'}"
                       ng-repeat="c in gantt.headers.day | ganttColumnLimit:scroll_start:scroll_width track by $index"
                       ng-click="raiseColumnDateClickedEvent($event, c)"
@@ -64,7 +64,7 @@
                     {{ headerFormatDay && (c.date | ganttDate:headerFormatDay) || (viewScale === 'hour' && (c.date | ganttDate: 'dd EEEE') || (c.date | ganttDate:'dd')) }}
                 </span>
             </div>
-            <div class="gantt-head-row" ng-if="headerShowHour && gantt.headers.hour !== undefined">
+            <div class="gantt-head-row" ng-if="gantt.headers.hour !== undefined">
                 <span ng-style="{'width': c.width+'em', 'left': c.left+'em'}"
                       ng-repeat="c in gantt.headers.hour | ganttColumnLimit:scroll_start:scroll_width track by $index"
                       ng-click="raiseColumnDateClickedEvent($event, row)"

commit ad06a2036cead6f0104b679dbf342f58f7c2d03e

fix(header): Show day header on hour view and week & month headers on day view
Depending on the current view scale the day or week & month is additionally shown in the header. Otherwise it is impossible to see from day numbers only what the current month or year is.

This commit fixes a regression introduced with d83f0e3dafd92b33dff5df9915d1967cfd5b7536

assets/angular-gantt.js
@@ -1754,10 +1754,10 @@ gantt.factory('HeaderGenerator', [ 'Column', 'moment', function(Column, moment) 
                 if ($scope.headerShowDay && ['hour', 'day'].indexOf($scope.viewScale) > -1) {
                     headers.day = generateDayHeader(columns);
                 }
-                if ($scope.headerShowWeek && ['week'].indexOf($scope.viewScale) > -1) {
+                if ($scope.headerShowWeek && ['day', 'week'].indexOf($scope.viewScale) > -1) {
                     headers.week = generateWeekHeader(columns);
                 }
-                if ($scope.headerShowMonth && ['week', 'month'].indexOf($scope.viewScale) > -1) {
+                if ($scope.headerShowMonth && ['day', 'week', 'month'].indexOf($scope.viewScale) > -1) {
                     headers.month = generateMonthHeader(columns);
                 }
                 return headers;

demo/app/index.html
@@ -178,7 +178,8 @@
                                                current-date="options.currentDate"
                                                current-date-value="options.currentDateValue"
                                                max-height="options.maxHeight && 300 || 0"
-                                               on-scroll="scrollEvent(event)">
+                                               on-scroll="scrollEvent(event)"
+                                               header-format-day="options.scale === 'hour' && 'LL' || 'D'">
                                         </gantt>
                                     </div>
                                 </div>

src/logic/headerGenerator.factory.js
@@ -79,10 +79,10 @@ gantt.factory('HeaderGenerator', [ 'Column', 'moment', function(Column, moment) 
                 if ($scope.headerShowDay && ['hour', 'day'].indexOf($scope.viewScale) > -1) {
                     headers.day = generateDayHeader(columns);
                 }
-                if ($scope.headerShowWeek && ['week'].indexOf($scope.viewScale) > -1) {
+                if ($scope.headerShowWeek && ['day', 'week'].indexOf($scope.viewScale) > -1) {
                     headers.week = generateWeekHeader(columns);
                 }
-                if ($scope.headerShowMonth && ['week', 'month'].indexOf($scope.viewScale) > -1) {
+                if ($scope.headerShowMonth && ['day', 'week', 'month'].indexOf($scope.viewScale) > -1) {
                     headers.month = generateMonthHeader(columns);
                 }
                 return headers;