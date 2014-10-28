60721e05b1c92b314a8da9f0a919728838f7fab0
Toilal <toilal.dev@gmail.com>
2014-10-28 15:35:15 +0100

    fix(demo): Remove bootstrap.js from demo
    This removes bootstrap.js, as it's useless and it produce error because jquery is missing from demo.

--- demo/Gruntfile.js
+++ demo/Gruntfile.js
@@ -165,13 +165,13 @@ module.exports = function (grunt) {
       app: {
         src: ['<%= yeoman.app %>/index.html'],
         ignorePath:  /\.\.\//,
-        exclude: [/bower_components\/jquery/]
+        exclude: [/bower_components\/jquery/, /bower_components\/bootstrap\/.*\.js/]
       },
       test: {
           devDependencies: true,
           src: 'test/karma.conf.js',
           ignorePath:  /\.\.\//,
-          exclude: [/bower_components\/jquery/],
+          exclude: [/bower_components\/jquery/, /bower_components\/bootstrap\/.*\.js/],
           fileTypes: {
               js: {
                   block: /(([\s\t]*)\/{2}\s*?bower:\s*?(\S*))(\n|\r|.)*?(\/{2}\s*endbower)/gi,
