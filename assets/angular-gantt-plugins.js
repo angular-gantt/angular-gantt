angular-gantt/angular-gantt

commit 70fc3ff4982c531cf9155e5c3eff8f88083177df

bug(columns): Generate columns on data load only if necessary
In case new tasks are added the columns need to be generated only if the tasks from/to values are outside of the current columns date range.

assets/angular-gantt.js
@@ -1217,7 +1217,11 @@ gantt.factory('GanttColumnsManager', ['GanttColumnGenerator', 'GanttHeaderGenera
         });
 
         this.gantt.api.data.on.load(this.gantt.$scope, function() {
-            self.generateColumns();
+            if (self.from > self.gantt.rowsManager.getDefaultFrom() ||
+                self.to < self.gantt.rowsManager.getDefaultTo()) {
+                self.generateColumns();
+            }
+
             self.gantt.rowsManager.sortRows();
         });
 
@@ -3752,7 +3756,7 @@ gantt.factory('ganttSmartEvent', [function() {
 }]);
 angular.module('ganttTemplates', []).run(['$templateCache', function($templateCache) {
     $templateCache.put('template/default.gantt.tmpl.html',
-        '<div class="gantt unselectable" gantt-scroll-manager gantt-element-width-listener>\n' +
+        '<div class="gantt unselectable" ng-cloak gantt-scroll-manager gantt-element-width-listener>\n' +
         '    <gantt-labels>\n' +
         '        <div class="gantt-labels-header">\n' +
         '            <gantt-row-header></gantt-row-header>\n' +

src/core/logic/column/columnsManager.factory.js
@@ -39,7 +39,11 @@ gantt.factory('GanttColumnsManager', ['GanttColumnGenerator', 'GanttHeaderGenera
         });
 
         this.gantt.api.data.on.load(this.gantt.$scope, function() {
-            self.generateColumns();
+            if (self.from > self.gantt.rowsManager.getDefaultFrom() ||
+                self.to < self.gantt.rowsManager.getDefaultTo()) {
+                self.generateColumns();
+            }
+
             self.gantt.rowsManager.sortRows();
         });
 

demo/dist/fonts/glyphicons-halflings-regular.svg
@@ -226,4 +226,4 @@
 <glyph unicode="&#xe199;" d="M100 200h400v-155l-75 -45h350l-75 45v155h400l-270 300h170l-270 300h170l-300 333l-300 -333h170l-270 -300h170z" />
 <glyph unicode="&#xe200;" d="M121 700q0 -53 28.5 -97t75.5 -65q-4 -16 -4 -38q0 -74 52.5 -126.5t126.5 -52.5q56 0 100 30v-306l-75 -45h350l-75 45v306q46 -30 100 -30q74 0 126.5 52.5t52.5 126.5q0 24 -9 55q50 32 79.5 83t29.5 112q0 90 -61.5 155.5t-150.5 71.5q-26 89 -99.5 145.5 t-167.5 56.5q-116 0 -197.5 -81.5t-81.5 -197.5q0 -4 1 -11.5t1 -11.5q-14 2 -23 2q-74 0 -126.5 -52.5t-52.5 -126.5z" />
 </font>
-</defs></svg>
+</defs></svg> 

commit 429c7b21ee77fb6d88df9eace17867c865b9bc4e

fix(columns): Generate column on first load
Optimization provided by 70fc3ff4982c531cf9155e5c3eff8f88083177df caused columns to be not generated when from-date and on-date are undefined.

assets/angular-gantt-plugins.js
@@ -6,18 +6,6 @@ License: MIT.
 Github: https://github.com/angular-gantt/angular-gantt
 */
 'use strict';
-angular.module('gantt.0.templates', []).run(['$templateCache', function($templateCache) {
-
-}]);
-
-angular.module('gantt.1.templates', []).run(['$templateCache', function($templateCache) {
-
-}]);
-
-angular.module('gantt.2.templates', []).run(['$templateCache', function($templateCache) {
-
-}]);
-
 angular.module('gantt.bounds.templates', []).run(['$templateCache', function($templateCache) {
     $templateCache.put('plugins/bounds/default.taskBounds.tmpl.html',
         '<div ng-show="bounds && isTaskMouseOver" class="gantt-task-bounds" ng-style="getCss()" ng-class="getClass()">\n' +
@@ -27,6 +15,12 @@ angular.module('gantt.bounds.templates', []).run(['$templateCache', function($te
 
 angular.module('gantt.movable.templates', []).run(['$templateCache', function($templateCache) {
 
+}]);
+
+angular.module('gantt.progress.templates', []).run(['$templateCache', function($templateCache) {
+    $templateCache.put('plugins/progress/default.taskProgress.tmpl.html',
+        '<div ng-cloak class=\'gantt-task-progress\' ng-style="getCss()" ng-class="task.progress.classes"></div>\n' +
+        '');
 }]);
 
 angular.module('gantt.sortable.templates', []).run(['$templateCache', function($templateCache) {

assets/angular-gantt.js
@@ -1219,7 +1219,8 @@ gantt.factory('GanttColumnsManager', ['GanttColumnGenerator', 'GanttHeaderGenera
         });
 
         this.gantt.api.data.on.load(this.gantt.$scope, function() {
-            if (self.from > self.gantt.rowsManager.getDefaultFrom() ||
+            if (self.from === undefined || self.to === undefined ||
+                self.from > self.gantt.rowsManager.getDefaultFrom() ||
                 self.to < self.gantt.rowsManager.getDefaultTo()) {
                 self.generateColumns();
             }
@@ -1681,7 +1682,7 @@ gantt.factory('Gantt', [
         // Adds or update rows and tasks.
         Gantt.prototype.loadData = function(data) {
             if (!angular.isArray(data)) {
-                data = [data];
+                data = data !== undefined ? [data] : [];
             }
 
             for (var i = 0, l = data.length; i < l; i++) {
@@ -1695,7 +1696,7 @@ gantt.factory('Gantt', [
         // If a row has no tasks inside the complete row will be deleted.
         Gantt.prototype.removeData = function(data) {
             if (!angular.isArray(data)) {
-                data = [data];
+                data = data !== undefined ? [data] : [];
             }
 
             this.rowsManager.removeData(data);
@@ -2707,23 +2708,27 @@ gantt.filter('ganttTaskLimit', [function() {
 
     return function(input, gantt) {
         var res = [];
-        for (var i = 0, l = input.length; i < l; i++) {
-            var task = input[i];
-            // If the task can be drawn with gantt columns only.
-            if (task.to > gantt.columnsManager.getFirstColumn().date && task.from < gantt.columnsManager.getLastColumn().endDate) {
+        var firstColumn = gantt.columnsManager.getFirstColumn();
+        var lastColumn = gantt.columnsManager.getLastColumn();
 
-                var scrollLeft = gantt.$scope.scrollLeft;
-                var scrollWidth = gantt.$scope.scrollWidth;
+        if (firstColumn !== undefined && lastColumn !== undefined) {
+            for (var i = 0, l = input.length; i < l; i++) {
+                var task = input[i];
+                // If the task can be drawn with gantt columns only.
+                if (task.to > gantt.columnsManager.getFirstColumn().date && task.from < gantt.columnsManager.getLastColumn().endDate) {
 
-                // If task has a visible part on the screen
-                if (task.left >= scrollLeft && task.left <= scrollLeft + scrollWidth ||
-                    task.left + task.width >= scrollLeft && task.left + task.width <= scrollLeft + scrollWidth ||
-                    task.left < scrollLeft && task.left + task.width > scrollLeft + scrollWidth) {
+                    var scrollLeft = gantt.$scope.scrollLeft;
+                    var scrollWidth = gantt.$scope.scrollWidth;
 
-                    res.push(task);
+                    // If task has a visible part on the screen
+                    if (task.left >= scrollLeft && task.left <= scrollLeft + scrollWidth ||
+                        task.left + task.width >= scrollLeft && task.left + task.width <= scrollLeft + scrollWidth ||
+                        task.left < scrollLeft && task.left + task.width > scrollLeft + scrollWidth) {
+
+                        res.push(task);
+                    }
                 }
             }
-
         }
 
         return res;

demo/dist/scripts/vendor.js
@@ -36793,7 +36793,8 @@ gantt.factory('GanttColumnsManager', ['GanttColumnGenerator', 'GanttHeaderGenera
         });
 
         this.gantt.api.data.on.load(this.gantt.$scope, function() {
-            if (self.from > self.gantt.rowsManager.getDefaultFrom() ||
+            if (self.from === undefined || self.to === undefined ||
+                self.from > self.gantt.rowsManager.getDefaultFrom() ||
                 self.to < self.gantt.rowsManager.getDefaultTo()) {
                 self.generateColumns();
             }
@@ -37255,7 +37256,7 @@ gantt.factory('Gantt', [
         // Adds or update rows and tasks.
         Gantt.prototype.loadData = function(data) {
             if (!angular.isArray(data)) {
-                data = [data];
+                data = data !== undefined ? [data] : [];
             }
 
             for (var i = 0, l = data.length; i < l; i++) {
@@ -37269,7 +37270,7 @@ gantt.factory('Gantt', [
         // If a row has no tasks inside the complete row will be deleted.
         Gantt.prototype.removeData = function(data) {
             if (!angular.isArray(data)) {
-                data = [data];
+                data = data !== undefined ? [data] : [];
             }
 
             this.rowsManager.removeData(data);
@@ -38281,23 +38282,27 @@ gantt.filter('ganttTaskLimit', [function() {
 
     return function(input, gantt) {
         var res = [];
-        for (var i = 0, l = input.length; i < l; i++) {
-            var task = input[i];
-            // If the task can be drawn with gantt columns only.
-            if (task.to > gantt.columnsManager.getFirstColumn().date && task.from < gantt.columnsManager.getLastColumn().endDate) {
+        var firstColumn = gantt.columnsManager.getFirstColumn();
+        var lastColumn = gantt.columnsManager.getLastColumn();
 
-                var scrollLeft = gantt.$scope.scrollLeft;
-                var scrollWidth = gantt.$scope.scrollWidth;
+        if (firstColumn !== undefined && lastColumn !== undefined) {
+            for (var i = 0, l = input.length; i < l; i++) {
+                var task = input[i];
+                // If the task can be drawn with gantt columns only.
+                if (task.to > gantt.columnsManager.getFirstColumn().date && task.from < gantt.columnsManager.getLastColumn().endDate) {
 
-                // If task has a visible part on the screen
-                if (task.left >= scrollLeft && task.left <= scrollLeft + scrollWidth ||
-                    task.left + task.width >= scrollLeft && task.left + task.width <= scrollLeft + scrollWidth ||
-                    task.left < scrollLeft && task.left + task.width > scrollLeft + scrollWidth) {
+                    var scrollLeft = gantt.$scope.scrollLeft;
+                    var scrollWidth = gantt.$scope.scrollWidth;
 
-                    res.push(task);
+                    // If task has a visible part on the screen
+                    if (task.left >= scrollLeft && task.left <= scrollLeft + scrollWidth ||
+                        task.left + task.width >= scrollLeft && task.left + task.width <= scrollLeft + scrollWidth ||
+                        task.left < scrollLeft && task.left + task.width > scrollLeft + scrollWidth) {
+
+                        res.push(task);
+                    }
                 }
             }
-
         }
 
         return res;
@@ -39358,18 +39363,6 @@ License: MIT.
 Github: https://github.com/angular-gantt/angular-gantt
 */
 'use strict';
-angular.module('gantt.0.templates', []).run(['$templateCache', function($templateCache) {
-
-}]);
-
-angular.module('gantt.1.templates', []).run(['$templateCache', function($templateCache) {
-
-}]);
-
-angular.module('gantt.2.templates', []).run(['$templateCache', function($templateCache) {
-
-}]);
-
 angular.module('gantt.bounds.templates', []).run(['$templateCache', function($templateCache) {
     $templateCache.put('plugins/bounds/default.taskBounds.tmpl.html',
         '<div ng-show="bounds && isTaskMouseOver" class="gantt-task-bounds" ng-style="getCss()" ng-class="getClass()">\n' +
@@ -39379,6 +39372,12 @@ angular.module('gantt.bounds.templates', []).run(['$templateCache', function($te
 
 angular.module('gantt.movable.templates', []).run(['$templateCache', function($templateCache) {
 
+}]);
+
+angular.module('gantt.progress.templates', []).run(['$templateCache', function($templateCache) {
+    $templateCache.put('plugins/progress/default.taskProgress.tmpl.html',
+        '<div ng-cloak class=\'gantt-task-progress\' ng-style="getCss()" ng-class="task.progress.classes"></div>\n' +
+        '');
 }]);
 
 angular.module('gantt.sortable.templates', []).run(['$templateCache', function($templateCache) {

src/core/logic/column/columnsManager.factory.js
@@ -41,7 +41,8 @@ gantt.factory('GanttColumnsManager', ['GanttColumnGenerator', 'GanttHeaderGenera
         });
 
         this.gantt.api.data.on.load(this.gantt.$scope, function() {
-            if (self.from > self.gantt.rowsManager.getDefaultFrom() ||
+            if (self.from === undefined || self.to === undefined ||
+                self.from > self.gantt.rowsManager.getDefaultFrom() ||
                 self.to < self.gantt.rowsManager.getDefaultTo()) {
                 self.generateColumns();
             }

src/core/logic/gantt.factory.js
@@ -111,7 +111,7 @@ gantt.factory('Gantt', [
         // Adds or update rows and tasks.
         Gantt.prototype.loadData = function(data) {
             if (!angular.isArray(data)) {
-                data = [data];
+                data = data !== undefined ? [data] : [];
             }
 
             for (var i = 0, l = data.length; i < l; i++) {
@@ -125,7 +125,7 @@ gantt.factory('Gantt', [
         // If a row has no tasks inside the complete row will be deleted.
         Gantt.prototype.removeData = function(data) {
             if (!angular.isArray(data)) {
-                data = [data];
+                data = data !== undefined ? [data] : [];
             }
 
             this.rowsManager.removeData(data);

src/core/ui/limit/taskLimit.filter.js
@@ -5,23 +5,27 @@ gantt.filter('ganttTaskLimit', [function() {
 
     return function(input, gantt) {
         var res = [];
-        for (var i = 0, l = input.length; i < l; i++) {
-            var task = input[i];
-            // If the task can be drawn with gantt columns only.
-            if (task.to > gantt.columnsManager.getFirstColumn().date && task.from < gantt.columnsManager.getLastColumn().endDate) {
+        var firstColumn = gantt.columnsManager.getFirstColumn();
+        var lastColumn = gantt.columnsManager.getLastColumn();
 
-                var scrollLeft = gantt.$scope.scrollLeft;
-                var scrollWidth = gantt.$scope.scrollWidth;
+        if (firstColumn !== undefined && lastColumn !== undefined) {
+            for (var i = 0, l = input.length; i < l; i++) {
+                var task = input[i];
+                // If the task can be drawn with gantt columns only.
+                if (task.to > gantt.columnsManager.getFirstColumn().date && task.from < gantt.columnsManager.getLastColumn().endDate) {
 
-                // If task has a visible part on the screen
-                if (task.left >= scrollLeft && task.left <= scrollLeft + scrollWidth ||
-                    task.left + task.width >= scrollLeft && task.left + task.width <= scrollLeft + scrollWidth ||
-                    task.left < scrollLeft && task.left + task.width > scrollLeft + scrollWidth) {
+                    var scrollLeft = gantt.$scope.scrollLeft;
+                    var scrollWidth = gantt.$scope.scrollWidth;
 
-                    res.push(task);
+                    // If task has a visible part on the screen
+                    if (task.left >= scrollLeft && task.left <= scrollLeft + scrollWidth ||
+                        task.left + task.width >= scrollLeft && task.left + task.width <= scrollLeft + scrollWidth ||
+                        task.left < scrollLeft && task.left + task.width > scrollLeft + scrollWidth) {
+
+                        res.push(task);
+                    }
                 }
             }
-
         }
 
         return res;