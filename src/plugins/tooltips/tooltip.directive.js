cb0a42f7f9bbdfef4a12b2894b4a2a8b3c48511a
Toilal <toilal.dev@gmail.com>
2015-01-17 08:55:47 +0100

    fix(tooltips): Add better checkings to hide tooltip
    Close #317

--- src/plugins/tooltips/tooltip.directive.js
+++ src/plugins/tooltips/tooltip.directive.js
@@ -63,6 +63,15 @@
                         mouseEnterX = e.clientX;
                         displayTooltip(true, false);
                     } else {
+                        // check if mouse goes outside the parent
+                        if(
+                            e.clientX < $scope.taskRect.left ||
+                            e.clientX > $scope.taskRect.right ||
+                            e.clientY > $scope.taskRect.bottom ||
+                            e.clientY < $scope.taskRect.top
+                        ) {
+                            displayTooltip(false, false);
+                        }
                         updateTooltip(e.clientX);
                     }
                 }, 5, false));
