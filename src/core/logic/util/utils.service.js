14bbeb8ff5ea616c468e2cf93032c0b55a287aaf
Toilal <toilal.dev@gmail.com>
2015-02-02 08:35:23 +0100

    fix(ie): Use `visibility: hidden` instead of `pointer-events: none` for findElementFromPoint.
    pointer-events is not supported by IE<11, causing findElementFromPoint to return only the visible layer element.
    
    Close #327

--- src/core/logic/util/utils.service.js
+++ src/core/logic/util/utils.service.js
@@ -1,6 +1,6 @@
 (function() {
     'use strict';
-    angular.module('gantt').service('ganttUtils', ['$document', function($document) {
+    angular.module('gantt').service('ganttUtils', [function() {
         return {
             createBoundedWrapper: function(object, method) {
                 return function() {
@@ -17,69 +17,6 @@
                     }
                 }
                 return defaultValue;
-            },
-            elementFromPoint: function(x, y) {
-                return $document[0].elementFromPoint(x, y);
-            },
-            elementsFromPoint: function(x, y, depth) {
-                var elements = [], previousPointerEvents = [], cDepth = 0, current, i, l, d;
-
-                // get all elements via elementFromPoint, and remove them from hit-testing in order
-                while ((current = this.elementFromPoint(x, y)) && elements.indexOf(current) === -1 && current !== null &&
-                    (depth === undefined || cDepth < depth)) {
-
-                    // push the element and its current style
-                    elements.push(current);
-                    previousPointerEvents.push({
-                        value: current.style.getPropertyValue('pointer-events'),
-                        priority: current.style.getPropertyPriority('pointer-events')
-                    });
-
-                    // add "pointer-events: none", to get to the underlying element
-                    current.style.setProperty('pointer-events', 'none', 'important');
-
-                    cDepth++;
-                }
-
-                // restore the previous pointer-events values
-                for (i = 0, l = previousPointerEvents.length; i < l; i++) {
-                    d = previousPointerEvents[i];
-                    elements[i].style.setProperty('pointer-events', d.value ? d.value : '', d.priority);
-                }
-
-                return elements;
-            },
-            findElementFromPoint: function(x, y, checkFunction) {
-                var elements = [], previousPointerEvents = [], cDepth = 0, current, found, i, l, d;
-
-                // get all elements via elementFromPoint, and remove them from hit-testing in order
-                while ((current = this.elementFromPoint(x, y)) && elements.indexOf(current) === -1 && current !== null) {
-
-                    // push the element and its current style
-                    elements.push(current);
-                    previousPointerEvents.push({
-                        value: current.style.getPropertyValue('pointer-events'),
-                        priority: current.style.getPropertyPriority('pointer-events')
-                    });
-
-                    // add "pointer-events: none", to get to the underlying element
-                    current.style.setProperty('pointer-events', 'none', 'important');
-
-                    cDepth++;
-
-                    if (checkFunction(current)) {
-                        found = current;
-                        break;
-                    }
-                }
-
-                // restore the previous pointer-events values
-                for (i = 0, l = previousPointerEvents.length; i < l; i++) {
-                    d = previousPointerEvents[i];
-                    elements[i].style.setProperty('pointer-events', d.value ? d.value : '', d.priority);
-                }
-
-                return found;
             },
             random4: function() {
                 return Math.floor((1 + Math.random()) * 0x10000)
