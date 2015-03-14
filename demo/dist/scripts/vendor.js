837700190019956b2f02c49dd70e2d6de3544754
Toilal <toilal.dev@gmail.com>
2015-03-14 09:42:29 +0100

    refactor(css): Clean up CSS files

--- demo/dist/scripts/vendor.js
+++ demo/dist/scripts/vendor.js
@@ -34205,7 +34205,7 @@ angular.module("ang-drag-drop",[])
 				            });
 			            }
 
-			            e.dataTransfer.setData("dataToSend", sendData);
+			            e.dataTransfer.setData("text", sendData);
 			            currentData = angular.fromJson(sendData);
 			            e.dataTransfer.effectAllowed = "copyMove";
 			            $rootScope.$broadcast("ANGULAR_DRAG_START", sendChannel, currentData.data);
@@ -34307,7 +34307,7 @@ angular.module("ang-drag-drop",[])
                         e.stopPropagation(); // Necessary. Allows us to drop.
                     }
 
-                    var sendData = e.dataTransfer.getData("dataToSend");
+                    var sendData = e.dataTransfer.getData("text");
                     sendData = angular.fromJson(sendData);
 
                     // Chrome doesn't set dropEffect, so we have to work it out ourselves
