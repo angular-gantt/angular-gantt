9f97ed06a4b9f73f88eee3306e770a8ed4a0efab
Toilal <toilal.dev@gmail.com>
2015-12-15 07:48:06 +0100

    chore(build): Remove cdnify from demo build

--- demo/Gruntfile.js
+++ demo/Gruntfile.js
@@ -308,13 +308,6 @@ module.exports = function (grunt) {
       }
     },
 
-    // Replace Google CDN references
-    cdnify: {
-      dist: {
-        html: ['<%= yeoman.dist %>/*.html']
-      }
-    },
-
     // Copies remaining files to places other tasks can use
     copy: {
       dist: {
@@ -424,7 +417,6 @@ module.exports = function (grunt) {
     'concat',
     'ngAnnotate',
     'copy:dist',
-    'cdnify',
     'cssmin',
     //'uglify',
     //'filerev',
